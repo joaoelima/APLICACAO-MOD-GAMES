5128c7c56cc46de7e820a8c0532561465050e092
João Eduardo Lima <106280952+joaoelima@users.noreply.github.com>
2026-09-01 23:01:18 -0300

    feat: adicionar consulta de regiões de memória

--- src/GameTrainer.Core/Memory/NativeMethods.cs
+++ src/GameTrainer.Core/Memory/NativeMethods.cs
@@ -13,6 +13,37 @@ internal static class NativeMethods
         QueryInformation = 0x0400
     }
 
+    [Flags]
+    internal enum MemoryProtection : uint
+    {
+        NoAccess = 0x01,
+        ReadOnly = 0x02,
+        ReadWrite = 0x04,
+        WriteCopy = 0x08,
+        Execute = 0x10,
+        ExecuteRead = 0x20,
+        ExecuteReadWrite = 0x40,
+        ExecuteWriteCopy = 0x80,
+        Guard = 0x100
+    }
+
+    internal enum MemoryState : uint
+    {
+        Commit = 0x1000
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct MemoryBasicInformation
+    {
+        public IntPtr BaseAddress;
+        public IntPtr AllocationBase;
+        public MemoryProtection AllocationProtect;
+        public UIntPtr RegionSize;
+        public MemoryState State;
+        public MemoryProtection Protect;
+        public uint Type;
+    }
+
     [DllImport("kernel32.dll", SetLastError = true)]
     internal static extern IntPtr OpenProcess(ProcessAccess access, bool inheritHandle, int processId);
 
@@ -33,6 +64,13 @@ internal static class NativeMethods
         byte[] buffer,
         int size,
         out IntPtr bytesWritten);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    internal static extern UIntPtr VirtualQueryEx(
+        IntPtr processHandle,
+        IntPtr address,
+        out MemoryBasicInformation buffer,
+        UIntPtr length);
 
     [DllImport("kernel32.dll", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
